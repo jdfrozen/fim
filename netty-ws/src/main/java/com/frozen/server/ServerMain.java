90443691fcf97a087331264cb8b58f3ce2b576fa
jdfrozen <616769416@qq.com>
2019-08-07 09:56:42 +0800

    添加netty-ws

---
+++ netty-ws/src/main/java/com/frozen/server/ServerMain.java
@@ -0,0 +1,37 @@
+package com.frozen.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author: Frozen
+ * @create: 2019-08-07 09:43
+ * @description:
+ **/
+public class ServerMain {
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup wokerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,wokerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .option(ChannelOption.SO_KEEPALIVE,true)
+                    .childHandler(new WebSocketChannelInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(new InetSocketAddress(8899)).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            wokerGroup.shutdownGracefully();
+        }
+    }
+}
